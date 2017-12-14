2b384596a6aa52822014de18bc2001808ab3f6e2
Khursheed Ali <khursheedali38@gmail.com>
2017-12-14 22:44:52 +0530

    added contact page view only

--- personal/views.py
+++ personal/views.py
@@ -1,5 +1,10 @@
+"""This script helps us define whatever user can view and what to render when user requests are received"""
+
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'personal/home.html') #automatically detects templates folder
+    return render(request, 'personal/home.html') 
+
+def contact(request):
+    return render(request, 'personal/basic.html', {'content':['If you would like to contact me regarding any project related discussions. Please email me.', 'khursheedali38@gmail.com']})
