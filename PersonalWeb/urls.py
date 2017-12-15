d5d3b1334ee5e2ecb0b32a994ff819c31d836d99
Khursheed Ali <khursheedali38@gmail.com>
2017-12-15 15:58:58 +0530

    added blog

--- PersonalWeb/urls.py
+++ PersonalWeb/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^webapp/', include('webapp.urls')),
     url(r'^', include('personal.urls')),
+    url(r'^blog/', include('blog.urls')),
+
 ]
