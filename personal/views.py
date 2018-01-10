21c6d81089981ea13b719c09823900e46e17470e
Khursheed Ali <khursheedali38@gmail.com>
2018-01-10 10:02:30 +0530

    added pdf rendering

--- personal/views.py
+++ personal/views.py
@@ -1,10 +1,16 @@
 """This script helps us define whatever user can view and what to render when user requests are received"""
 
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'personal/home.html') 
 
 def tutorials(request):
     return render(request, 'personal/basic.html', {'content':['staty tunedIf you would like to contact me regarding any project related discussions. Please email me.', 'khursheedali38@gmail.com']})
+
+def resume(request):
+    file = open('personal/templates/personal/documents/CV_Updated.pdf', 'rb')
+    response = HttpResponse(file, content_type='application/pdf')
+    response['Content-Disposition'] = 'filename=CV_Updated.pdf'
+    return response
