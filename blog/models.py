afa859c470f96df99b35a23d69ec8c4a5ff4e70a
Khursheed Ali <khursheedali38@gmail.com>
2018-01-13 09:55:51 +0530

    build v1.3

--- blog/models.py
+++ blog/models.py
@@ -3,9 +3,12 @@ from django.db import models
 # Create your models here.
 	
 class Post(models.Model):
-    title = models.CharField(max_length = 200)
+    category = models.CharField(max_length = 50)
+    title = models.CharField(max_length = 100)
+    overview = models.TextField()
     body = models.TextField()
     date = models.DateTimeField()
+
 
     def __str__(self):
         return self.title
